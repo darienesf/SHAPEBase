17c030a63061ad5bb3ca835ec4b4e40ad8669d68
Dar ien <29084453+darienesf@users.noreply.github.com>
2019-08-02 09:44:52 -0400

    Create cables.java

---
+++ cables.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class inventory {
+
+private int count;
+public String color;
+public int length
+protected String cate;
+
+}
+
+public inventory()
+{
+count = newCount; //this is the amount in stock
+color = newColor; //color of cable
+length = newLength; //length of cable
+cate = newCategory; //different instances based on ARRAY
+
+}
+
+public static void setCount(int count)
+{
+int newCount = count;
+}
+public static getCount(int count)
+{
+return newCount;
+}
+
+public static void setColor(String color)
+{
+String color = cabColor
+}
+public static void main(String[] args) {
+Scanner keyboard = new Scanner(System.in);
+
+
+/*
+NEEDS OUTLINE
+*/
+
+}
